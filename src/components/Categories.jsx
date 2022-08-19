2788565dc05920319a03223cb12d779ff0921553
DmitriyKovchugo <dkovchugo@mail.ru>
2022-08-19 10:42:15 +0300

    1-5 lessons

---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Categories() {
+    const [activeIndex, setActiveIndex] = React.useState(0)
+
+    const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые']
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((value,index) => (
+                    <li key={index} onClick={() => onClickCategory(index)} className={activeIndex === index ? "active" : ''}>{value}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
