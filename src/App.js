31e65135b62c77d2015ae2f61bc5dd7f42e098e4
DmitriyKovchugo <dkovchugo@mail.ru>
2022-10-18 16:17:16 +0300

    feat: add context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NotFound from "./pages/NotFound";
 import {Routes, Route} from "react-router-dom";
 import Cart from "./pages/Cart";
 
+export const SearchContext = React.createContext();
+
 function App() {
 
     const [searchValue, setSearchValue] = React.useState('');
@@ -15,14 +17,16 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
-        <div className="content">
-            <Routes>
-              <Route path={'/'} element={<Home searchValue={searchValue}/>} />
-              <Route path={'/cart'} element = {<Cart />}/>
-              <Route path={'*'} element={<NotFound />} />
-            </Routes>
-          </div>
+          <SearchContext.Provider value={{searchValue, setSearchValue}}>
+              <Header />
+              <div className="content">
+                  <Routes>
+                      <Route path={'/'} element={<Home searchValue={searchValue}/>} />
+                      <Route path={'/cart'} element = {<Cart />}/>
+                      <Route path={'*'} element={<NotFound />} />
+                  </Routes>
+              </div>
+          </SearchContext.Provider>
       </div>
     </div>
   );
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import classes from './Search.module.scss'
+import {SearchContext} from "../../App";
 
-const Search = ({searchValue, setSearchValue}) => {
+const Search = () => {
+    const {searchValue, setSearchValue} = React.useContext(SearchContext)
+
     return (
         <div className={classes.root}>
             <svg className={classes.icon}
